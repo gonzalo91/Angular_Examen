6e7e0ebb66bbcb59b532f842343b8bf2c62f7c77
Gonzalo <gonzalo.adriancr@gmail.com>
2020-11-13 17:42:53 -0600

    Table User Component Set Up

---
+++ src/app/usuarios/usuarios.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { TablaUsuariosComponent } from '../components/tabla-usuarios/tabla-usuarios.component'
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap : [TablaUsuariosComponent]
+
+})
+
+
+export class UsuariosModule { }
